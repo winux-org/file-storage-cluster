a59fbc11e4ca1e6c3112d03b68b0ea55941c6fe5
Artur Gurgul <artur.gurgul@vattenfall.com>
2020-12-03 17:30:21 +0100

    a

--- database.go
+++ database.go
@@ -17,20 +17,23 @@ func GenerateFileHash() string {
 }
 
 func ConnectToDatabase() {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	//conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:password@localhost:5430/files")
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 	defer conn.Close(context.Background())
 
-	var name string
-	var weight int64
-	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	var id int64
+	//var weight int64
+	//err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	err = conn.QueryRow(context.Background(), "select * from files").Scan(&id)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(name, weight)
+	fmt.Println("id", id)
 }
--- serv.go
+++ serv.go
@@ -4,8 +4,11 @@ import (
 	"log"
 )
 
+// docker run -p 5672:5672 -d --hostname storage-rabbit --name storage-rabbit rabbitmq:3
+// docker run -p 5430:5432 --name postgres89  -e POSTGRES_PASSWORD=password -d postgres
+
 func main() {
-	//ConnectToDatabase()
+	ConnectToDatabase()
 	SetupRPC()
 
 	log.Printf(" [*] File cluster started")
